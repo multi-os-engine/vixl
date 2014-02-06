b70f53637d105d94bc6541844c655bbe558b7245
Serban Constantinescu <serban.constantinescu@arm.com>
2014-02-06 15:30:50 +0000

    VIXL: Add Android build support
    * Android.mk file and logcat support.
    * MODULE_LICENSE
    
    Change-Id: I357177baa12d211b82830f0931f1ea54565f167b
    Signed-off-by: David Butcher <david.butcher@arm.com>
    Signed-off-by: Serban Constantinescu <serban.constantinescu@arm.com>

---
+++ Android.mk
@@ -0,0 +1,52 @@
+LOCAL_PATH:= $(call my-dir)
+
+vixl_include_files := $(LOCAL_PATH)/src/ \
+
+vixl_src_files := \
+		src/utils.cc \
+		src/a64/disasm-a64.cc \
+		src/a64/assembler-a64.cc \
+		src/a64/instructions-a64.cc \
+		src/a64/debugger-a64.cc \
+		src/a64/macro-assembler-a64.cc \
+		src/a64/decoder-a64.cc \
+		src/a64/simulator-a64.cc \
+		src/a64/instrument-a64.cc
+
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_CPPFLAGS := -O2 -Wall -Wextra
+LOCAL_C_INCLUDES := $(vixl_include_files)
+LOCAL_SRC_FILES :=  $(vixl_src_files)
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libvixl
+include external/stlport/libstlport.mk
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_CPPFLAGS := -O2 -Wall -Wextra -DDEBUG=1
+LOCAL_C_INCLUDES := $(vixl_include_files)
+LOCAL_SRC_FILES :=  $(vixl_src_files)
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libvixlg
+include external/stlport/libstlport.mk
+include $(BUILD_SHARED_LIBRARY)
+
+# We need -m64 for 64bit Host builds
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_CPPFLAGS := -O2 -Wall -Wextra
+ifeq ($(TARGET_IS_64_BIT), true)
+LOCAL_CPPFLAGS += -m64
+endif
+
+LOCAL_C_INCLUDES := $(vixl_include_files)
+LOCAL_SRC_FILES :=  $(vixl_src_files)
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libvixl
+include $(BUILD_HOST_STATIC_LIBRARY)
+
