f44e764cddc601ccee532ac3f9cae46a68b6121c
armvixl <vixl@arm.com>
2016-06-24 10:38:39 +0100

    Integrate new 1.13 release of VIXL.
    - Compiles 32/64-bit support into separate -arm and -arm64 targets.
    
    - Relies on "Fixes to integrate new version of VIXL." in art repo to
      fix ARM64 backend to compile with new interface.
    
    Change-Id: I55ec62e024b43862d6e8cda1417b988a657bd13c

--- test/test-invalset.cc
+++ test/test-invalset.cc
@@ -24,9 +24,8 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include "invalset-vixl.h"
 #include "test-runner.h"
-
-#include "vixl/invalset.h"
 
 namespace vixl {
 
